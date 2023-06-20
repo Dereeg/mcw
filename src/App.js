25253fb5084cfb1903d17a5fb3f9f236509a4aad
Ricardo Macagnan <ricardo.macagnan@gmail.com>
2023-06-20 17:18:38 -0400

    Added data fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify, API } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const [response, setResponse] = useState({});
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    Amplify.configure({ 
+      API: {
+        endpoints: [
+          {
+              name: "mcwAPIdev", // name of the API in API Gateway console
+              endpoint: "https://qqfpbgvu96.execute-api.us-east-1.amazonaws.com/dev/",
+              region: "eu-east-1",
+              paths: ['/']
+          }
+      ]
+      }
+    })
+    API.configure()
+
+    const getData = async () => {
+      const apiName = 'mcwAPIdev';
+      const path = '/mcw';
+      const myInit = {
+        headers: {} // OPTIONAL
+      };
+      const resp = await API.get(apiName, path, myInit)
+    
+      setResponse(resp);
+    }
+    getData().catch(console.error)
+ 
+  }, [response]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className="Data">{ JSON.stringify(response, null, 2) } </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
